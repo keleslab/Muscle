0c74f23c1d44434c1b9d433577f19990474f82ba
kmp0223 <68722153+kmp0223@users.noreply.github.com>
2023-12-04 20:44:04 -0600

    Update config_file_model.R

--- code/wrapper/config_file_model.R
+++ code/wrapper/config_file_model.R
@@ -1,28 +1,22 @@
 
-
-
 #Directory of data: The directory should contain hic_df.qs and chrom.sizes file.
-#dir_data="/storage08/kwangmoon/Muscle/data/Tan2021"
-dir_data="/storage08/kwangmoon/Muscle/data/Lee2019/500kb"
+dir_data="{Muscle directory}/data/Liu2021"
 
 #Directory of functions
-dir_functions="/storage08/kwangmoon/Muscle/code/functions"
+dir_functions="{Muscle directory}/code/functions"
 
 #Directory where Muscle output goes into
 
-#dir_out="/storage08/kwangmoon/Muscle/results/Tan2021"
-dir_out="/storage08/kwangmoon/Muscle/results/Lee2019/500kb/schic"
-
+dir_out="{Muscle directory}/results/Liu2021"
 
 
 #Initial maximum rank of Methylation SVD
 exploration_rank=150
 
-
 #Modality of the tensors. #(only 3 choices available : "All", or "HiC", or "HiC+CG")
 
-#modality="All" #When all Hi-C, mCG, mCH are analyzed
-modality="HiC" #When only Hi-C is analyzed
+modality="All" #When all Hi-C, mCG, mCH are analyzed
+#modality="HiC" #When only Hi-C is analyzed
 #modality="HiC+CG" #when only Hi-C and mCG are analyzed
 
 
@@ -31,10 +25,9 @@ modality="HiC" #When only Hi-C is analyzed
 #( 0 (default) : it means to choose rank based on the screeplot of SVD, e.g., Rank_HiC=0 means that the rank for Hi-C data is chosen based on SVD of Hi-C.)
 #( R : it means to set the rank as R, e.g., Rank_HiC=30 means that the rank for Hi-C data is 30. R is nonzero in this case.)
 
-Rank_HiC=50
-Rank_mCG=30
+Rank_HiC=0
+Rank_mCG=0
 Rank_mCH=0
-
 
 
 
@@ -43,8 +36,8 @@ Rank_mCH=0
 Bulk_exist=FALSE
 
 #If GNU parllel exists, type TRUE. Otherwise, FALSE. 
-GNU=TRUE
-#GNU=FALSE
+#GNU=TRUE
+GNU=FALSE
 
 #tolerance level for each rank 1 update 
 tol=0.00001
@@ -55,6 +48,7 @@ maxiter=10
 
 #server names for gnu parallel. 
 #If you are not using multiple servers for GNU parallel, leave it as ssh=NULL
-#If multiple servers can be used, list them with comma without space. 
-#e.g., ssh='server01,server02,server03'
-ssh='hodor01,hodor02'
+ssh=NULL
+
+#If multiple servers can be used, list them with comma without space. e.g., 'server01,server02'
+#ssh='server01,server02,server03'
